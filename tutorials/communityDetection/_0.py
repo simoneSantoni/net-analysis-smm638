617e27c8aa1c6789703a038afc73d26b2395adf8
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-10-25 23:45:12 +0000

    Initial commit.

---
+++ tutorials/communityDetection/_0.py
@@ -0,0 +1,13 @@
+# !/usr/env/bin python3
+#-*- encoding is utf-8 -*-
+
+""""
+--------------------------------------------------------------------------------
+    Node centrality metrics
+--------------------------------------------------------------------------------
+Author: Simone Santoni, simone.santoni.1@city.ac.uk
+Dates:  - created 17/10/2020, 23:05:11
+        - last change 17/10/2020, 23:05:37
+Notes: This script illustrates three popular centrality metrics, such as
+       node degree, eigenvector centrality, and betweenness centrality
+"""
